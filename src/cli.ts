0fffc81bad66af779c5e0e683095e0ef6f11f687
Olivier Goulet <olivier_goulet@icloud.com>
2026-06-27 12:28:15 -0400

    feat: initial commit

---
+++ src/cli.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+const { version } = require("../package.json") as { version: string };
+
+interface RunOptions {
+  args: string[];
+}
+
+async function run(this: void, _options: RunOptions): Promise<void> {
+  console.log("lit-ssg running...");
+}
+
+const program = new Command();
+
+program
+  .name("lit-ssg")
+  .description("Render Lit web components in generated SSG outputs")
+  .version(version)
+  .action(async (options) => {
+    try {
+      await run(options);
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+
+program.parse();
